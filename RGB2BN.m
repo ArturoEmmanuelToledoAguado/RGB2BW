5625aa27b287df097ea3b96937efcbb5350b2008
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-08-23 17:29:59 -0500

    Extraccion de los planos RGB

--- RGB2BN.m
+++ RGB2BN.m
@@ -4,5 +4,8 @@ clear all;
 
 %Extracción de la imagen
 img=imread('Gera.jpg');
-[f c]=size(img);
-%imshow(img)
+[f c]=size(img);%Tamaño de la imagen
+%Extraccion de planos R (Red), G (Green) y B (Blue) a manita :D
+img_R(:,:,1)=img(:,:,1);
+img_G(:,:,1)=img(:,:,2);
+img_B(:,:,1)=img(:,:,3);
