0c61379b7bc22525a79d640f42b7b873abc37c44
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2023-04-05 19:52:53 -0600

    No logre usar la formula NTSC

--- RGB2BN.py
+++ RGB2BN.py
@@ -14,18 +14,28 @@ Por defecto openCV abre los ficheros en BGR (no en RGB)
 """
 cv2.imshow("Gera",img)
 
+#Convertimos de BGR a RGB
+img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
 #Dimensiones de la imagen y creacion de las matrices
 #[f,c,p]=img.shape
+#print(img.shape)
 
 #Descomponemos la imagen en sus planos R:Rojo, G:Verde, B:Azul
-R=img[:,:,2]
+R=img[:,:,0]
 G=img[:,:,1]
-B=img[:,:,0]
+B=img[:,:,2]
 
 #Mostramos los planos RGB
 cv2.imshow('R',R)
 cv2.imshow('G',G)
 cv2.imshow('B',B)
+
+#Convertimos a escala de grises
+img_BW=cv2.imread('Gera.jpg',0)
+
+#Mostramos la imagen
+cv2.imshow('Imagen en escala de grises',img_BW)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
